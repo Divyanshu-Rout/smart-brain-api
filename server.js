5dd7e2f5e34a2ee083d89abfb8dcf4f3d0295dc0
Divyanshu-Rout <divyanshukumarrout@gmail.com>
2020-09-23 23:32:58 -0700

    smart-brain-api

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db  = knex ({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user:'postgres',
+        password:'div',
+        database: 'smartbrain'
+    }
+});
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+    users:[
+        {
+            id: '123',
+            name:'john',
+            email:'john@gmail.com',
+            password: 'cokkies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name:'div',
+            email:'div@gmail.com',
+            password: 'cok',
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+    login: {
+        id: '125',
+        name:'d',
+        email:'d@gmail.com',
+        has: '',
+    }
+}
+
+app.get('/',(req,res)=> {
+    res.send(database.users)
+})
+
+app.post('/signin', signin.handleSignin( db, bcrypt));
+
+app.post('/register',(req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id',(req, res) => {
+    profile.handleProfile(req, res, db)
+})
+
+app.put('/image',(req, res) => {
+    image.handleImage(req, res, db)
+})
+app.post('/imageurl',(req, res) => {
+    image.handleApiCall(req, res)
+})
+
+
+
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000')
+})
